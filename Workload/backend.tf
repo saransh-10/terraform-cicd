7565e9bf39468a2b7569b6d9641d962133f2d719
Saranshkamboj <90630979+Saranshkamboj@users.noreply.github.com>
2024-11-13 00:08:25 +0530

    Update backend.tf

--- Workload/backend.tf
+++ Workload/backend.tf
@@ -10,8 +10,3 @@ terraform {
     subscription_id      = "${var.ARM_SUBSCRIPTION_ID}"
   }
 }
-
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "ARM_TENANT_ID" {}
-variable "ARM_SUBSCRIPTION_ID" {}
