7017d70bce1683bc606747a492cf698cd88f6ac0
Saranshkamboj <90630979+Saranshkamboj@users.noreply.github.com>
2024-11-19 17:15:23 +0530

    Update locals.tf

--- Workload/locals.tf
+++ Workload/locals.tf
@@ -112,7 +112,7 @@ locals {
     offer     = "0001-com-ubuntu-server-jammy"
   }
   # Databricks Locals
-  storage_account_name             = lower("${var.org_name}${var.bu_name}dls291")
+  storage_account_name             = lower("${var.org_name}${var.bu_name}dls292")
   storage_container_metastore_name = lower("metastore")
   storage_container_ext_name       = lower("external")
   access_connector_name            = "${var.org_name}-${var.bu_name}-dbwaccbg"
